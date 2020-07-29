ef771830d98e04e3b2b013bfba590216623db587
denicCH <com-08@yandex.ru>
2020-07-29 13:40:06 +0300

    Add files via upload

---
+++ Lesson_8/query.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+mongo_base = client.instagramru
+collection = mongo_base['instagram']
+
+
+def pr_mongo(items):
+    for key, item in items.items():
+        print(key)
+        for it in item:
+            print(it)
+
+
+subscriber = collection.find({'subscriber': {"$eq": 22212229670}})  # //те кто подписаны на пользователя
+subscriptions = collection.find({'subscriptions': {"$eq": 22212229670}})  # //те на кого подписан пользователь
+r = {
+    'subscriber': collection.find({'subscriber': {"$eq": 22212229670}}),  # //те кто подписаны на пользователя
+    'subscriptions': collection.find({'subscriptions': {"$eq": 22212229670}})  # //те на кого подписан пользователь
+}
+
+pr_mongo(r)
